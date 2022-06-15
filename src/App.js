56cf5ecb84801841b4c7adf4d5685fa4f4a3dc6d
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-06-15 18:24:31 +0530

    added Procts pages by ak and shoheab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
+import AllFruits from "./Pages/Products/AllFruits";
+import FreshCuts from "./Pages/Products/FreshCuts";
+import FruitCombos from "./Pages/Products/FruitCombos";
+import GiftsByKimaye from "./Pages/Products/GiftsByKimaye";
 
 function App() {
   return (
@@ -10,6 +14,24 @@ function App() {
       <Navbar></Navbar>
       <HomePage></HomePage>
       <Footer></Footer>
+      <Routes>
+        <Route
+          path="collections/all-fruits"
+          element={<AllFruits></AllFruits>}
+        ></Route>
+        <Route
+          path="collections/fresh-cuts"
+          element={<FreshCuts></FreshCuts>}
+        ></Route>
+        <Route
+          path="collections/fruit-combos"
+          element={<FruitCombos></FruitCombos>}
+        ></Route>
+        <Route
+          path="collections/gifts-by-kimaye"
+          element={<GiftsByKimaye></GiftsByKimaye>}
+        ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Products/GiftsByKimaye.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GiftsByKimaye = () => {
+  return (
+    <div>GiftsByKimaye</div>
+  )
+}
+
+export default GiftsByKimaye
