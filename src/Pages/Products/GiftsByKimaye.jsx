1a0ddf0a0533d36dc682ce058e0e08a653a9b6eb
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-06-15 19:35:43 +0530

    Products added by AK

--- src/Pages/Products/GiftsByKimaye.jsx
+++ src/Pages/Products/GiftsByKimaye.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGiftsByKimaye } from "../../Store/Products/products.action";
 
 const GiftsByKimaye = () => {
-  return (
-    <div>GiftsByKimaye</div>
-  )
-}
+  const dispatch = useDispatch();
+  const { giftsbykimaye } = useSelector((state) => state.products);
+  console.log("giftsbykimaye:", giftsbykimaye);
 
-export default GiftsByKimaye
+  useEffect(() => {
+    dispatch(getGiftsByKimaye());
+  }, [dispatch]);
+  return <div>GiftsByKimaye</div>;
+};
+
+export default GiftsByKimaye;
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,20 @@
-import React from 'react'
-import {legacy_createStore,applyMiddleware,combineReducers,compose} from "redux"
-import thunk from "react-thunk"
-import { productreducer } from './productdetail/product.reducer';
+import {
+  legacy_createStore,
+  applyMiddleware,
+  combineReducers,
+  compose,
+} from "redux";
+import thunk from "redux-thunk";
+import { productreducer } from "./productdetail/product.reducer";
+import { productsReducer } from "./Products/products.reducer";
 
 const composeEnhancer = window.__Redux_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let rootreducer= combineReducers({
-    product:productreducer
-})
+let rootreducer = combineReducers({
+  product: productreducer, // by shashank
+  products: productsReducer, // by Akshay
+});
 
-export const store = legacy_createStore(rootreducer,composeEnhancer(applyMiddleware(thunk)));
+export const store = legacy_createStore(
+  rootreducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
